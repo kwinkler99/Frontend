d35162d9ba9f7557bade49ced3106d33ee134c41
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-10-06 20:23:07 +0200

    Lab01 - zad02

--- lab01/src/Formularz.js
+++ lab01/src/Formularz.js
@@ -4,21 +4,37 @@ import React, {useState} from "react";
 const Formularz = () => {
     const [text, setText] = useState("");
     const [list, setList] = useState([]);
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(Date(Date.now()));
     const [warning, setWarning] = useState("");
+    const months = {
+        'Jan' : '01',
+        'Feb' : '02',
+        'Mar' : '03',
+        'Apr' : '04',
+        'May' : '05',
+        'Jun' : '06',
+        'Jul' : '07',
+        'Aug' : '08',
+        'Sep' : '09',
+        'Oct' : '10',
+        'Nov' : '11',
+        'Dec' : '12'
+    }
 
     
 
 
     function addText_to_List(event) {
-        let now = Date.now();
-        if(text !== "" && date !== ""){
+        let now = Date(Date.now()).split(' ')
+        let compare = now[3]+"-"+months[now[1]]+"-"+ now[2]
+
+        if(text !== "" && date !== "" && Date.parse(compare) < Date.parse(date)){
             setList([...list, {text, date}]);
             setDate("");
             setText("");
             setWarning("");
         }
-        else if(Date.parse(date) < now){
+        else if(Date.parse(compare) >= Date.parse(date)){
             setWarning("UWAGA! Podano zla date");
         }
         else{
