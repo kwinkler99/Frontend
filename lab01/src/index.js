063eff28c29a1651319372009af6b71ff92a2b60
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-10-22 02:49:43 +0200

    Wyczyszczone pliki

--- lab01/src/index.js
+++ lab01/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Formularz from './Formularz.js'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Formularz />
+  </div>,
   document.getElementById('root')
 );
-
-
-serviceWorker.unregister();
