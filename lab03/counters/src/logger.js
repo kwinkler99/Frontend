38d903085e99a9d66a6acb16b6fd8b642e5a71cb
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-11-03 09:57:22 +0100

    lab05 - zad02

--- lab03/counters/src/logger.js
+++ lab03/counters/src/logger.js
@@ -1,4 +1,7 @@
 export const logger = store => next => action => {
+    if(action.type === "INCREMENT"){
+        alert("liczba zostanie zwiekszona o 1")
+    }
     console.log(action)
     next(action);
 }
