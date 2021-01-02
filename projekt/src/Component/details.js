728417f1214aee01f6bf52b092dd90b1678a8f21
Katarzyna Winkler <kasia.winkler99@gmail.com>
2021-01-02 11:30:38 +0100

    add css to page with product details

--- projekt/src/Component/details.js
+++ projekt/src/Component/details.js
@@ -7,18 +7,35 @@ class Details extends Component {
 
     render(){
         const {product} = this.props.data
+
         return (
             <div className="product">
-                <img src={product.image} alt={product.name} />
-                <p>{product.name}</p>
-                <p>{product.brand}</p>
-                <p>{product.price}</p>
-                <p>{product.currency}</p>
-                <p>{product.description}</p>
-                <p>{product.category}</p>
-                <p>{product.type}</p>
-                <p>{product.tag}</p>
-                {product.colors.map(clr => <div key={clr.hex_value} className="color" style={{backgroundColor:clr.hex_value}}></div>)}
+                <img className="imgDetails" src={product.image} alt={product.name} />
+                <div className="textAlign">
+                    <p className="textDetails">Name: </p>
+                    <p>{product.name}</p>
+                    <p className="textDetails">Brand: </p>
+                    <p>{product.brand}</p>
+                    <p className="textDetails">Price: </p>
+                    <p>{product.price}</p>
+                    <p className="textDetails">Currency: </p>
+                    <p>{product.currency}</p>
+                    <p className="textDetails">Description: </p>
+                    <p>{product.description}</p>
+                    <p className="textDetails">Category: </p>
+                    <p>{product.category}</p>
+                    <p className="textDetails">Type: </p>
+                    <p>{product.type}</p>
+                    <p className="textDetails">Tags: </p>
+                    <p>{product.tag}</p>
+                    <p className="textDetails">Colors: </p>
+                    {product.colors.map(clr => 
+                        <div 
+                            key={clr.colour_name} 
+                            className="color" 
+                            style={{backgroundColor:clr.hex_value}}>
+                        </div>)}
+                </div>
             </div>
         )
     }
